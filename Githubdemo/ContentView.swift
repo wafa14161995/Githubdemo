e75fc15c93aeadaa5999c4ebfefbaaddb7546130
wafa14161995 <Wafa14161995@gmail.com>
2024-09-26 06:57:47 -0700

    Initial Commit

---
+++ Githubdemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Githubdemo
+//
+//  Created by Wafa Awad  on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Githubdemo/GithubdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubdemoApp.swift
+//  Githubdemo
+//
+//  Created by Wafa Awad  on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct GithubdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
