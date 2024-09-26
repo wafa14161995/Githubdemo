7c7d6495fb78baef642ebe119a7ee28ecd5d0988
wafa14161995 <Wafa14161995@gmail.com>
2024-09-26 07:38:04 -0700

    water tracker

--- Githubdemo/ContentView.swift
+++ Githubdemo/ContentView.swift
@@ -10,10 +10,24 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Water Tracker💦")
+                .fontWeight(.bold)
+                .font(.largeTitle)
+            Divider()
+            HStack{
+                
+                
+                Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                    Text("Apple heath")
+                }
+            }
+            Divider()
+            Stepper(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(4)/*@END_MENU_TOKEN@*/, in: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Range@*/1...10/*@END_MENU_TOKEN@*/) {
+                Text(" Cups to drink per day 0")
+                     }
+            Button("Continue") {
+                
+            }
         }
         .padding()
     }
